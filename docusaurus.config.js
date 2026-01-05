9a7d60dae83df3c0b6726e94dd31512738f170ac
Stuzanna <66328800+Stuzanna@users.noreply.github.com>
2026-01-05 21:59:41 +0000

    Update footer for project

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,7 +115,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Weekly Mileage Tracker. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
